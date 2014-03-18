6d7b793229f0d5c73a9d623a05eb7b055e0be149
Nic Wayand <wayandn@hydro-c1.rap.ucar.edu>
2014-03-18 06:04:16 +0000

    extract_forcign_at_point.sh outputs all variables, need to fix time and remove blank line, add loop

--- extract_forcing_at_point.sh
+++ extract_forcing_at_point.sh
@@ -2,8 +2,44 @@
 
 
 maindir="/d1/wayandn/Grid_data/maurer12k"
-echo $maindir
+tempdir="/d1/wayandn/Grid_data/tempfiles/"
+outpdir="/d1/wayandn/Grid_data/CSSL_pt/"
 
+# Clear
+rm -f $outpdir"*"
 
-#ncks -s '%13.9f\n' -C -H -d lat,1,1 -d lon,1,1 -d time,0,3 -v ppt ./maurer12k/MAURER12K_Forcing.1988-10.nc
+#Lat lon indices (zero based)
+Ilat=100
+Ilon=100
+
+FL=$maindir"/MAU*"
+echo $FL
+
+# for each file
+#for cf in $FL
+#do
+
+# Need to output time in a correct way
+ncdump -v var1,var2 foo.nc | sed -e '1,/data:/d' -e '$d' 
+# check to remove black space
+
+	ncdump -t -v time $FL > $tempdir"time"
+	ncks -s '%13.9f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v ppt $FL > $tempdir"ppt"
+	ncks -s '%13.3f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v temp $FL > $tempdir"temp"
+	ncks -s '%13.9f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v q $FL > $tempdir"q"
+	ncks -s '%13.9f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v press $FL > $tempdir"press"
+	ncks -s '%13.3f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v sw $FL > $tempdir"sw"
+	ncks -s '%13.3f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v lw $FL > $tempdir"lw"
+	ncks -s '%13.3f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v wnd $FL > $tempdir"wnd"
+
+	cat $tempdir"time" >> $outpdir"time"
+	cat $tempdir"ppt" >> $outpdir"ppt"
+	cat $tempdir"temp" >> $outpdir"temp"
+	cat $tempdir"q" >> $outpdir"q"
+	cat $tempdir"press" >> $outpdir"press"
+	cat $tempdir"sw" >> $outpdir"sw"
+	cat $tempdir"lw" >> $outpdir"lw"
+	cat $tempdir"wnd" >> $outpdir"wnd"
+#done
+
 
