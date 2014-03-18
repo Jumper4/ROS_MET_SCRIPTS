a5c4716cd7a7be492e443b7b598a3d0fa1cd6769
Nic Wayand <wayandn@hydro-c1.rap.ucar.edu>
2014-03-18 04:55:03 +0000

    Created file to extract forcing from one grid cell

---
+++ extract_forcing_at_point.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+maindir="/d1/wayandn/Grid_data/maurer12k"
+echo $maindir
+
+
+#ncks -s '%13.9f\n' -C -H -d lat,1,1 -d lon,1,1 -d time,0,3 -v ppt ./maurer12k/MAURER12K_Forcing.1988-10.nc
+
