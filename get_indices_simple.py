4fcef96d55569e838ffe79c73d069bf8a15fcbb7
Nic Wayand <wayandn@hydro-c1.rap.ucar.edu>
2014-03-24 23:59:55 +0000

    now takes a file of lat lons and returns a file of index, ned to check

--- get_indices_simple.py
+++ get_indices_simple.py
@@ -1,10 +1,13 @@
 import numpy as np
 import netCDF4
 
-t_lat = 39.313237
-t_lon = -120.394202
+#t_lat = 39.313237
+#t_lon = -120.394202
 
-print 'We want lat an lon values:',t_lat,t_lon
+filelistin  = open('/d1/wayandn/Grid_data/Basin_pts/Lat_Lon_Low_Basin.txt','r')
+filelistout = open('/d1/wayandn/Grid_data/Basin_pts/Lat_Lon_Low_Basin_Index.txt','w')
+
+#print 'We want lat an lon values:',t_lat,t_lon
 
 def naive_fast(latvar,lonvar,lat0,lon0):
     # Read latitude and longitude from file into numpy arrays
@@ -14,7 +17,7 @@ def naive_fast(latvar,lonvar,lat0,lon0):
     dist_sq = (latvals-lat0)**2 + (lonvals-lon0)**2
     minindex_flattened = dist_sq.argmin()  # 1D index of min element
     iy_min,ix_min = np.unravel_index(minindex_flattened, latvals.shape)
-    return iy_min,ix_min
+    return str(iy_min),str(ix_min)
 
 filename = '/d1/wayandn/Grid_data/maurer12k/MAURER12K_Forcing.1994-01.nc'
 ncfile = netCDF4.Dataset(filename, 'r')
@@ -24,7 +27,17 @@ newlat = latvar * np.ones((len(lonvar),len(latvar)))
 newlon = lonvar * np.ones((len(latvar),len(lonvar)))
 newlat = newlat.T
 newlon = (360 - newlon) * -1
-iy,ix = naive_fast(newlat,newlon, t_lat, t_lon)
-print 'Closest lat lon:', newlat[iy,ix], newlon[iy,ix]
-print 'index values for lat and lon are:', iy, ix
+
+for cl in filelistin:
+	#print cl
+	t_lon, t_lat = cl.split(',')
+	#print t_lon, t_lat
+	iy,ix = naive_fast(newlat,newlon, float(t_lat), float(t_lon))
+	#print iy, ix
+	filelistout.write("%s %s\n" % (iy, ix))	
+
+filelistout.close()
+filelistin.close()
+#print 'Closest lat lon:', newlat[iy,ix], newlon[iy,ix]
+#print 'index values for lat and lon are:', iy, ix
 ncfile.close()
