6eef1238e0d9dd811dc0e7def8b02b3e5ad14969
Jumper4 <nicway@u.washington.edu>
2014-03-27 22:53:48 +0000

    still updating

--- extract_forcing_at_point.sh
+++ extract_forcing_at_point.sh
@@ -6,24 +6,28 @@ maindir="/d1/wayandn/Grid_data/"
 datadir=$maindir"maurer12k/"
 BASIN="Low"
 
-# Clear all temp and output files
-while read FN Ilat Ilon cLat cLon tlat tlon
-do
-	outpdir=$maindir"Basin_pts/"$BASIN"/"$FN"/"	
-	cd $outpdir
-	rm -v time ppt temp q press sw lw wnd
-done < $I_lat_lon_list 
-echo Done Clearing up previous files
-
 #Lat lon indices (zero based)
 #Ilat=113
 #Ilon=34
 
-FL=$datadir"/MAU*"
+#FL=$datadir"/MAU*"
+FL=$datadir"MAURER12K_Forcing.1992-10.nc"
 I_lat_lon_list=$maindir"Basin_pts/"$BASIN"/"$BASIN".txt"
-echo $I_lat_lon_list
+#echo $I_lat_lon_list
+echo $FL
 
-#echo $FL
+# Clear all temp and output files
+while read FN Ilat Ilon cLat cLon tlat tlon
+do
+	ls
+        outpdir=$maindir"Basin_pts/"$BASIN"/"$FN"/"
+        rm $outpdir -v time ppt temp q press sw lw wnd
+        #rm -rv TEMP
+        #rm -rv OUT
+	ls
+done < $I_lat_lon_list
+echo Done Clearing up previous files
+
 
 for cf in $FL
 do
@@ -46,22 +50,22 @@ do
 		sed 's/    //g'    $tempdir"temp5" > $tempdir"temp6"
 		sed 's/;//g'       $tempdir"temp6" > $tempdir"time"
 	
-		#ncks -s '%13.9f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v ppt $cf > $tempdir"ppt"
-		#ncks -s '%13.3f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v temp $cf > $tempdir"temp"
-		#ncks -s '%13.9f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v q $cf > $tempdir"q"
-		#ncks -s '%13.9f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v press $cf > $tempdir"press"
-		#ncks -s '%13.3f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v sw $cf > $tempdir"sw"
-		#ncks -s '%13.3f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v lw $cf > $tempdir"lw"
-		#ncks -s '%13.3f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v wnd $cf > $tempdir"wnd"
+		ncks -s '%13.9f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v ppt $cf > $tempdir"ppt"
+		ncks -s '%13.3f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v temp $cf > $tempdir"temp"
+		ncks -s '%13.9f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v q $cf > $tempdir"q"
+		ncks -s '%13.9f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v press $cf > $tempdir"press"
+		ncks -s '%13.3f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v sw $cf > $tempdir"sw"
+		ncks -s '%13.3f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v lw $cf > $tempdir"lw"
+		ncks -s '%13.3f\n' -C -H -d lat,$Ilat,$Ilat -d lon,$Ilon,$Ilon -v wnd $cf > $tempdir"wnd"
 
 		cat $tempdir"time" >> $outpdir"time"
-		#head -n -1 $tempdir"ppt" >> $outpdir"ppt"
-		#head -n -1 $tempdir"temp" >> $outpdir"temp"
-		#head -n -1 $tempdir"q" >> $outpdir"q"
-		#head -n -1 $tempdir"press" >> $outpdir"press"
-		#head -n -1 $tempdir"sw" >> $outpdir"sw"
-		#head -n -1 $tempdir"lw" >> $outpdir"lw"
-		#head -n -1 $tempdir"wnd" >> $outpdir"wnd"
+		head -n -1 $tempdir"ppt" >> $outpdir"ppt"
+		head -n -1 $tempdir"temp" >> $outpdir"temp"
+		head -n -1 $tempdir"q" >> $outpdir"q"
+		head -n -1 $tempdir"press" >> $outpdir"press"
+		head -n -1 $tempdir"sw" >> $outpdir"sw"
+		head -n -1 $tempdir"lw" >> $outpdir"lw"
+		head -n -1 $tempdir"wnd" >> $outpdir"wnd"
 
 	done < $I_lat_lon_list
 done
@@ -70,13 +74,18 @@ done
 while read FN Ilat Ilon cLat cLon tlat tlon
 do
 	findir=$maindir"Basin_pts/"$BASIN"/"$FN"/OUT/"
+	comdir=$maindir"Basin_pts/"$BASIN"/ALL/"
 	mkdir -p $findir
+	mkdir -p $comdir
 
 	outpdir=$maindir"Basin_pts/"$BASIN"/"$FN"/"	
 	cd $outpdir
 
-	#paste time ppt temp q press sw lw wnd > $findir"forcing_"$FN".txt"
-	paste time > $findir"forcing_"$FN".txt"
+	paste time temp ppt q press wnd sw lw > $findir"forcing_"$FN".txt"
+	cp $findir"forcing_"$FN".txt" $comdir"forcing_"$FN".txt"
+	cp $outpdir"time" $comdir"time_"$FN".txt"
+
+	#paste time > $findir"forcing_"$FN".txt"
 
 done < $I_lat_lon_list
 
